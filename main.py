9c7d2fcd92a7a199577336b47b25364b67975cc0
Max <m.k4rp0v@yandex.ru>
2022-08-22 10:22:50 +0300

    upd

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-import schedule
 import clear_calls
 import ITS
 import GRM
 
 
 def main():
-    #clear_calls.main()
+    clear_calls.main()
     ITS.main()
-    #GRM.main()
+    GRM.main()
 
 main()
