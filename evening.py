9d40a62059f20582ec09a606cd12005a3a105309
Max <m.k4rp0v@yandex.ru>
2022-08-26 18:38:53 +0300

    ClearListRejections

--- evening.py
+++ evening.py
@@ -2,12 +2,14 @@
 import clear_calls
 import ITS
 import GRM
+import ClearListRejections
 
 
 def main():
     clear_calls.main()
     ITS.main()
     GRM.main()
+    ClearListRejections.main()
 
 
 main()
