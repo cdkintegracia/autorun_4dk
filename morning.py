6e6c2fdecbd9864270801d299f263e7f6c57fa55
Max <m.k4rp0v@yandex.ru>
2022-09-07 17:14:43 +0300

    312

--- morning.py
+++ morning.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-import tasks
-import ClearListBP
+import tasks, ClearListBP, X_Report
+
 
 
 def main():
     tasks.main()
     ClearListBP.clear_bp()
+    X_Report.main()
 
 
 main()
