2607ff481b554067c8f1b443ba29f863bc60ec15
korpovmoxem <m.k4rp0v@yandex.ru>
2022-09-13 12:59:48 +0300

    Update evening.py

--- evening.py
+++ evening.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*- 
-import clear_calls
-import ITS
-import GRM
+import clear_calls, ITS, GRM, X_Report
 
 
 def main():
     clear_calls.main()
     ITS.main()
     GRM.main()
+    X_Report.main()
+
 
 
 main()
