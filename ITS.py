de17fd05ad75c31b72acf84a81a4f69a717694cb
Max <m.k4rp0v@yandex.ru>
2022-11-16 10:42:06 +0300

    Update ITS.py

--- ITS.py
+++ ITS.py
@@ -239,9 +239,8 @@ def update_bitrix_list(report_type):
 
                                 for field_value in bitrix_element['PROPERTY_1287']:
                                     update_date = datetime.strptime(bitrix_element['PROPERTY_1287'][field_value], '%d.%m.%Y %H:%M:%S')
-                                    print(update_date, current_date, (current_date - update_date).days, bitrix_element['ID'])
                                     if (current_date - update_date).days > 5:
-                                        b.call('lists.element.get', {
+                                        b.call('lists.element.delete', {
                                     'IBLOCK_TYPE_ID': 'lists',
                                     'IBLOCK_ID': '169',
                                     'ELEMENT_ID': bitrix_element['ID']
