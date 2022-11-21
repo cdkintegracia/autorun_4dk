3117f4e4a5294bb82d22edc72c0ccc8daf93a7a7
Max <m.k4rp0v@yandex.ru>
2022-11-21 20:49:46 +0300

    Update EmailParcer.py

--- EmailParcer.py
+++ EmailParcer.py
@@ -1,4 +1,4 @@
-"""
+ """
 https://habr.com/ru/post/688784/
 """
 
@@ -87,7 +87,7 @@ def mail_parser():
                                 mail_header = decode_header(mail_info['Subject'])[0][0].decode()
                             else:
                                 mail_header = mail_info['Subject']
-
+                        print(type(mail_header))
                         if not any(True for allowed_header in allowed_mail_headers if allowed_header in mail_header):
                             continue
 
