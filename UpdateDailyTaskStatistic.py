763cb9647f9296550500f0111458ffa65a89dfa6
Max <m.k4rp0v@yandex.ru>
2023-01-19 17:55:41 +0300

    1

---
+++ UpdateDailyTaskStatistic.py
@@ -0,0 +1,109 @@
+from datetime import datetime, timedelta
+
+from fast_bitrix24 import Bitrix
+import gspread
+
+from authentication import authentication
+
+b = Bitrix(authentication('Bitrix'))
+month_int_names = {
+        1: 'Январь',
+        2: 'Февраль',
+        3: 'Март',
+        4: 'Апрель',
+        5: 'Май',
+        6: 'Июнь',
+        7: 'Июль',
+        8: 'Август',
+        9: 'Сентябрь',
+        10: 'Октябрь',
+        11: 'Ноябрь',
+        12: 'Декабрь',
+    }
+
+
+def get_tasks() -> dict:
+    task_tags = ['БП', 'ЗУП']
+    result = {}
+    current_date = datetime.now()
+    filter_date_b24_start = datetime.strftime(current_date, '%Y-%m-%d')
+    filter_date_b24_end = datetime.strftime(current_date + timedelta(days=1), '%Y-%m-%d')
+    result.setdefault('ЛК', {})
+    for tag in task_tags:
+        tasks = b.get_all('tasks.task.list', {
+            'filter': {
+                'TAG': tag,
+                '>=CREATED_DATE': filter_date_b24_start,
+                '<CREATED_DATE': filter_date_b24_end}
+        })
+        result['ЛК'].setdefault(tag, tasks)
+    tasks = b.get_all('tasks.task.list', {
+        'filter': {
+            'GROUP_ID': '1',
+            '>=CREATED_DATE': filter_date_b24_start,
+            '<CREATED_DATE': filter_date_b24_end
+        }})
+    result.setdefault('ТЛП', tasks)
+    return result
+
+
+def get_values_sum(row):
+    values_sum = 0
+    for ind in range(len(row)):
+        if ind == len(row) - 1:
+            break
+        value = row[ind]
+        if str(value).isdigit():
+            values_sum += int(value)
+    return str(values_sum)
+
+
+def update_daily_task_statistic():
+    tasks = get_tasks()
+    file_name = f'Задачи {datetime.now().year}'
+    sheet_name = month_int_names[datetime.now().month + 1]
+    try:
+        google_access = gspread.service_account(authentication('Google'))
+    except:
+        google_access = gspread.service_account(f"/root/credentials/{authentication('Google')}")
+    spreadsheet = google_access.open(file_name)
+    try:
+        worksheet = spreadsheet.add_worksheet(title=sheet_name, rows=1, cols=1)
+        titles = [
+            ['Группа', '', 'Всего'],
+            ['ЛК', ''],
+            ['', 'БП', ''],
+            ['', 'ЗУП', ''],
+            ['', 'Всего ЛК', ''],
+            [''],
+            ['ТЛП', 'Всего ТЛП', ]
+        ]
+        worksheet.update('A1', titles)
+    except gspread.exceptions.APIError:
+        worksheet = spreadsheet.worksheet(sheet_name)
+    worksheet_values = worksheet.get_all_values()
+    new_worksheet_data = []
+    for row in worksheet_values:
+        if 'Группа' in row:
+            current_date = datetime.strftime(datetime.now(), '%d.%m.%y')
+            row.insert(-1, current_date)
+        elif 'БП' in row:
+            row.insert(-1, len(tasks['ЛК']['БП']))
+        elif 'ЗУП' in row:
+            row.insert(-1, len(tasks['ЛК']['ЗУП']))
+        elif 'Всего ЛК' in row:
+            row.insert(-1, len(tasks['ЛК']['ЗУП']) + len(tasks['ЛК']['БП']))
+            row[-1] = get_values_sum(row)
+        elif 'ТЛП' in row:
+            row.insert(-1, len(tasks['ЛК']['ЗУП']) + len(tasks['ТЛП']))
+            row[-1] = get_values_sum(row)
+
+        new_worksheet_data.append(row)
+    worksheet.clear()
+    worksheet.update('A1', new_worksheet_data)
+
+
+if __name__ == '__main__':
+    update_daily_task_statistic()
+
+
--- evening.py
+++ evening.py
@@ -2,12 +2,14 @@
 import clear_calls, ITS, GRM, X_Report
 from SendNotification import send_motification
 from SendRequestCreateCurrentMonthDealsDataFile import send_request_create_current_month_deals_data_file
+from UpdateDailyTaskStatistic import update_daily_task_statistic
 
 
 def main():
     clear_calls.main()
     ITS.main()
     GRM.main()
+    update_daily_task_statistic()
     X_Report.main()
     send_request_create_current_month_deals_data_file()
     send_motification(['311', '1'], 'Автозапуск вечерних процессов завершен')
