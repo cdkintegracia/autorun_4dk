b221217405dc281ae0ae0e7c08882a6f74a0f7c8
Max <m.k4rp0v@yandex.ru>
2023-01-26 17:38:53 +0300

    1

--- evening.py
+++ evening.py
@@ -7,9 +7,13 @@ from UpdateDailyTaskStatistic import update_daily_task_statistic
 
 def main():
     clear_calls.main()
+    print('clear_calls завершено')
     update_daily_task_statistic()
+    print('update_daily_task_statistic завершено')
     send_request_create_current_month_deals_data_file()
+    print('send_request_create_current_month_deals_data_file завершено')
     X_Report.main()
+    print('X-report завершено')
     send_motification(['311', '1'], 'Автозапуск вечерних процессов завершен')
 
 
--- update_ITS_GRM.py
+++ update_ITS_GRM.py
@@ -5,7 +5,9 @@ from SendNotification import send_motification
 
 def main():
     ITS.main()
+    print('ITS завершено')
     GRM.main()
+    print('GRM завершено')
     send_motification(['311', '1'], 'Обновление ИТС и ГРМ завершено')
 
 main()
