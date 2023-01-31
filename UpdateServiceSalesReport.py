dc7246161222c629ecf70cd37ce70989aa3ae393
Max <m.k4rp0v@yandex.ru>
2023-01-31 14:19:09 +0300

    Update UpdateServiceSalesReport.py

--- UpdateServiceSalesReport.py
+++ UpdateServiceSalesReport.py
@@ -8,3 +8,7 @@ def update_service_sales_report():
     if current_day != 1:
         return
     requests.get(url='http://141.8.195.67:5000/update_service_sales_report')
+
+
+if __name__ == '__main__':
+    update_service_sales_report()
