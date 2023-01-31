22452e28cfe85e5cfb9e98423d1ee8c5eafa8a9c
Max <m.k4rp0v@yandex.ru>
2023-01-31 09:10:13 +0300

    Update update_ITS_GRM.py

--- update_ITS_GRM.py
+++ update_ITS_GRM.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 import ITS, GRM
 from SendNotification import send_motification
 
 
+
 def main():
+    week_day = datetime.today().isoweekday()
+    if week_day in [6, 7]:  # Выходные
+        return
     ITS.main()
     print('ITS завершено')
     GRM.main()
