06d1f9c34fa04748928b0b491ce9fac7fde09219
Max <m.k4rp0v@yandex.ru>
2023-02-15 10:51:44 +0300

    Update UpdateUserActivityStatistic.py

--- UpdateUserActivityStatistic.py
+++ UpdateUserActivityStatistic.py
@@ -67,6 +67,7 @@ def update_user_activity_statistic():
             titles.append(['', 'Исходящие звонки'])
             titles.append(['', 'Входящие звонки'])
             titles.append(['', 'Отправленные письма'])
+            titles.append(['', 'Обращений в 1С:Коннект'])
         worksheet.update('A1', titles)
     except gspread.exceptions.APIError:
         worksheet = spreadsheet.worksheet(sheet_name)
@@ -74,36 +75,39 @@ def update_user_activity_statistic():
     worksheet_values = worksheet.get_all_values()
     new_worksheet_data = []
 
-    date_filter = datetime.strftime(datetime.now(), '%Y-%m-%d')
-    calls = b.get_all('voximplant.statistic.get', {'filter': {'>CALL_START_DATE': date_filter, 'CALL_FAILED_CODE': '200'}})
     date_filter = datetime.strftime(datetime.now() - timedelta(days=1), '%Y-%m-%d')
-    sent_email = b.get_all('crm.activity.list', {'filter': {'PROVIDER_TYPE_ID': 'EMAIL', '>CREATED': date_filter, 'DIRECTION': '2'}})
-    tasks = b.get_all('tasks.task.list', {'filter': {'>CLOSED_DATE': date_filter}})
+    end_date_filter = datetime.strftime(datetime.now(), '%Y-%m-%d')
+    calls = b.get_all('voximplant.statistic.get', {'filter': {'>=CALL_START_DATE': date_filter, 'CALL_FAILED_CODE': '200', '<CALL_START_DATE': end_date_filter}})
+    sent_email = b.get_all('crm.activity.list', {'filter': {'PROVIDER_TYPE_ID': 'EMAIL', '>=CREATED': date_filter, 'DIRECTION': '2', '<CREATED': end_date_filter}})
+    tasks = b.get_all('tasks.task.list', {'filter': {'>=CLOSED_DATE': date_filter, '<CLOSED_DATE': end_date_filter}})
     user_name = ''
     user_id = ''
     for row in worksheet_values:
         activities_sum = ''
         if 'Пользователь' in row:
-            row.insert(-1, datetime.strftime(datetime.now(), '%d.%m.%Y'))
+            row.insert(-1, datetime.strftime(datetime.now() - timedelta(days=1), '%d.%m.%Y'))
         elif row[1] == '':
             user_name = row[0]
             user_id = list(filter(lambda x: x['NAME'] == user_name.split()[0] and x['LAST_NAME'] == user_name.split()[1], users_info))[0]['ID']
         elif 'Завершенные задачи' in row:
-            user_closed_tasks = list(filter(lambda x: x['responsibleId'] == user_id, tasks))
+            user_closed_tasks = list(filter(lambda x: x['responsibleId'] == user_id and '1С:Коннект' not in x['title'], tasks))
             row[-1] = (len(user_closed_tasks))
             activities_sum = sum(list(map(lambda x: int(x), row[2:])))
-            print(row)
         elif 'Исходящие звонки' in row:
-            user_outgoing_calls = list(filter(lambda x: x['PORTAL_USER_ID'] == user_id and int(x['CALL_DURATION']) > 10 and x['CALL_TYPE'] == '2', calls))
+            user_outgoing_calls = list(filter(lambda x: x['PORTAL_USER_ID'] == user_id and int(x['CALL_DURATION']) > 10 and x['CALL_TYPE'] == '1', calls))
             row[-1] = (len(user_outgoing_calls))
             activities_sum = sum(list(map(lambda x: int(x), row[2:])))
         elif 'Входящие звонки' in row:
-            user_incoming_calls = list(filter(lambda x: x['PORTAL_USER_ID'] == user_id and x['CALL_TYPE'] == '1', calls))
+            user_incoming_calls = list(filter(lambda x: x['PORTAL_USER_ID'] == user_id and x['CALL_TYPE'] == '2', calls))
             row[-1] = (len(user_incoming_calls))
             activities_sum = sum(list(map(lambda x: int(x), row[2:])))
         elif 'Отправленные письма' in row:
             user_emails = list(filter(lambda x: x['AUTHOR_ID'] == user_id, sent_email))
             row[-1] = (len(user_emails))
+            activities_sum = sum(list(map(lambda x: int(x), row[2:])))
+        elif 'Обращений в 1С:Коннект' in row:
+            closed_user_connect_tasks = list(filter(lambda x: x['responsibleId'] == user_id and '1С:Коннект' in x['title'], tasks))
+            row[-1] = (len(closed_user_connect_tasks))
             activities_sum = sum(list(map(lambda x: int(x), row[2:])))
         row.append(activities_sum)
         new_worksheet_data.append(row)
