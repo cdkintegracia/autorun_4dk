842e6a68d9ba48dc3e828265989a34cf2ed4d929
Max <m.k4rp0v@yandex.ru>
2023-03-16 15:30:06 +0300

    Update UpdateUserActivityStatistic.py

--- UpdateUserActivityStatistic.py
+++ UpdateUserActivityStatistic.py
@@ -102,25 +102,23 @@ def update_user_activity_statistic():
                 user_id = user_id[0]['ID']
             else:
                 user_id = ''
-        elif not user_id:
-            new_worksheet_data.append(row)
-        elif 'Завершенные задачи' in row:
+        elif 'Завершенные задачи' in row and user_id:
             user_closed_tasks = list(filter(lambda x: x['responsibleId'] == user_id and '1С:Коннект' not in x['title'], tasks))
             row[-1] = (len(user_closed_tasks))
             activities_sum = sum(list(map(lambda x: int(x), row[3:])))
-        elif 'Исходящие звонки' in row:
+        elif 'Исходящие звонки' in row and user_id:
             user_outgoing_calls = list(filter(lambda x: x['PORTAL_USER_ID'] == user_id and int(x['CALL_DURATION']) > 10 and x['CALL_TYPE'] == '1', calls))
             row[-1] = (len(user_outgoing_calls))
             activities_sum = sum(list(map(lambda x: int(x), row[3:])))
-        elif 'Входящие звонки' in row:
+        elif 'Входящие звонки' in row and user_id:
             user_incoming_calls = list(filter(lambda x: x['PORTAL_USER_ID'] == user_id and x['CALL_TYPE'] == '2', calls))
             row[-1] = (len(user_incoming_calls))
             activities_sum = sum(list(map(lambda x: int(x), row[3:])))
-        elif 'Отправленные письма' in row:
+        elif 'Отправленные письма' in row and user_id:
             user_emails = list(filter(lambda x: x['AUTHOR_ID'] == user_id, sent_email))
             row[-1] = (len(user_emails))
             activities_sum = sum(list(map(lambda x: int(x), row[3:])))
-        elif 'Обращений в 1С:Коннект' in row:
+        elif 'Обращений в 1С:Коннект' in row and user_id:
             closed_user_connect_tasks = list(filter(lambda x: x['responsibleId'] == user_id and '1С:Коннект' in x['title'], tasks))
             row[-1] = (len(closed_user_connect_tasks))
             activities_sum = sum(list(map(lambda x: int(x), row[3:])))
