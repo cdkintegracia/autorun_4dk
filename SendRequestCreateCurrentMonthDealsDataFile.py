a8eda6412ba3a1ce8aad843c51bde81b02ae4f9e
Max <m.k4rp0v@yandex.ru>
2023-06-30 18:06:17 +0300

    1

--- SendRequestCreateCurrentMonthDealsDataFile.py
+++ SendRequestCreateCurrentMonthDealsDataFile.py
@@ -4,13 +4,14 @@ from datetime import datetime
 import requests
 
 from SendNotification import send_notification
+from web_app_ip import web_app_ip
 
 
 def send_request_create_current_month_deals_data_file():
     month_days_range = monthrange(datetime.now().year, datetime.now().month)[1]
     if datetime.now().day != month_days_range:
         return
-    requests.get(url='http://141.8.195.67:5000/create_current_month_deals_data_file')
+    requests.get(url=f'{web_app_ip}/create_current_month_deals_data_file')
     send_notification(['311', '1'], 'Создан файл с информацией по сделкам для отчета по сумме сервисов')
 
 
