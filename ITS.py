b57c14a347088ca74d7f46fb6f8ed1027c62f94f
Max <m.k4rp0v@yandex.ru>
2023-07-03 15:48:32 +0300

    Update ITS.py

--- ITS.py
+++ ITS.py
@@ -211,6 +211,7 @@ def update_bitrix_list(report_type):
                         break
 
             startDate = tariff['startDate']  # Дата начала из отчета
+            print(startDate)
             startDate_formated = dateutil.parser.isoparse(startDate).strftime('%Y-%m-%d %H:%M:%S')
 
             # Поиск компании в Битриксе по ИНН из отчета
