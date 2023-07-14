7d69168f6165d6f9de5efb20d6903ded2815f44f
Max <m.k4rp0v@yandex.ru>
2023-07-14 12:40:38 +0300

    Update ITS.py

--- ITS.py
+++ ITS.py
@@ -259,6 +259,7 @@ def update_bitrix_list(report_type):
                     element_responsible = company['ASSIGNED_BY_ID']
 
                     filter_deals = list(filter(lambda x: x['COMPANY_ID'] == company['ID'], deals))
+                    print(tariff)
                     if 'тестовый' in tariff['name']:
                         filter_deals = True
                     print(filter_deals)
@@ -333,7 +334,7 @@ def update_bitrix_list(report_type):
 
                                     element_id = bitrix_element['ID']   # ID элемента списка
                                     #print('Id:', element_id)
-                                    print(element_id)
+
                                     b.call('lists.element.update',
                                            {
                                                'IBLOCK_TYPE_ID': 'lists',
