551b0b7e93280fe98c3742e29e30c29e36d6fe93
Max <m.k4rp0v@yandex.ru>
2023-07-17 03:56:34 +0300

    Update ITS.py

--- ITS.py
+++ ITS.py
@@ -456,22 +456,22 @@ report_types = {
 
 def main():
     notification_text = 'Элементы списка "Отчет по сервисам" обновлены'
-    #if datetime.today().isoweekday() == 1:
-        #update_bitrix_list('ESS')
-        #send_notification(['1', '311'], f'{notification_text} (Кабинет сотрудника)')
-    #else:
-    with open('/root/autorun_4dk/its_update.txt', 'r') as file:
-        day_code = int(file.read())
-    if day_code % 2 == 0:
-        update_bitrix_list('DOCUMENT_RECOGNITION')
-        #send_notification(['1', '311'], f'{notification_text} (РПД)')
+    if datetime.today().isoweekday() == 1:
+        update_bitrix_list('ESS')
+        send_notification(['1', '311'], f'{notification_text} (Кабинет сотрудника)')
     else:
-        update_bitrix_list('COUNTERAGENT')
-        #send_notification(['1', '311'], f'{notification_text} (Контрагент)')
+        with open('/root/autorun_4dk/its_update.txt', 'r') as file:
+            day_code = int(file.read())
+        if day_code % 2 == 0:
+            update_bitrix_list('DOCUMENT_RECOGNITION')
+            #send_notification(['1', '311'], f'{notification_text} (РПД)')
+        else:
+            update_bitrix_list('COUNTERAGENT')
+            #send_notification(['1', '311'], f'{notification_text} (Контрагент)')
 
-    day_code += 1
-    with open('/root/autorun_4dk/its_update.txt', 'w') as file:
-        file.write(str(day_code))
+        day_code += 1
+        with open('/root/autorun_4dk/its_update.txt', 'w') as file:
+            file.write(str(day_code))
 
     #delete_old_elements()
 
