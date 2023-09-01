257d3b2cb9b8c3e8a83465d404ed592ff909705a
Max <m.k4rp0v@yandex.ru>
2023-09-01 09:21:01 +0300

    1

--- SendRequestFillActDocumentSmartProcess.py
+++ SendRequestFillActDocumentSmartProcess.py
@@ -4,7 +4,8 @@ from web_app_ip import web_app_ip
 
 
 def send_request_fill_act_document_smart_process():
-    requests.get(url=f'{web_app_ip}/send_request_fill_act_document_smart_process')
+    for _ in range(2):
+        requests.get(url=f'{web_app_ip}/send_request_fill_act_document_smart_process')
 
 
 if __name__ == '__main__':
