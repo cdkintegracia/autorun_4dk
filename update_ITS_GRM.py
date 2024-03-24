15f0859c1b5e20a60b64df9a9a9e0eb3f637d2db
wtfetc <ib09@list.ru>
2024-03-24 14:28:45 +0300

    Update update_ITS_GRM.py

--- update_ITS_GRM.py
+++ update_ITS_GRM.py
@@ -10,10 +10,10 @@ def main():
     week_day = datetime.today().isoweekday()
     if week_day in [1,2,3,4,5,6]:  # Выходные
         return
-    try:
-        GRM.main()
-    except:
-        send_notification(['1'], 'Не удалось обновить УС "ГРМ"')
+    #try:
+    #    GRM.main()
+    #except:
+    #    send_notification(['1'], 'Не удалось обновить УС "ГРМ"')
     try:
         ITS.main()
     except:
