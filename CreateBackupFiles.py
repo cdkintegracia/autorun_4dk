6f526a4572373de1e328daf2cc4f607c8c90f58d
wtfetc <ib09@list.ru>
2024-05-04 14:44:45 +0300

    Добавлены поля для бэкапирования

--- CreateBackupFiles.py
+++ CreateBackupFiles.py
@@ -102,13 +102,13 @@ def main(filename, entity, companies, entity_id, folder_path, category_id):
 
 def create_backup_files():
     data_types = [
-        #{'filename': 'Сопровождение', 'entity': 'deal', 'entity_id': None, 'category_id': '1'},
-        #{'filename': 'Продажи', 'entity': 'deal', 'entity_id': None, 'category_id': '9'},
-        #{'filename': 'Адаптация', 'entity': 'deal', 'entity_id': None, 'category_id': '19'},
-        #{'filename': 'Источники_продаж', 'entity': 'item', 'entity_id': '133', 'category_id': None},
-        #{'filename': 'Досье_клиента', 'entity': 'item', 'entity_id': '186', 'category_id': None},
+        {'filename': 'Сопровождение', 'entity': 'deal', 'entity_id': None, 'category_id': '1'},
+        {'filename': 'Продажи', 'entity': 'deal', 'entity_id': None, 'category_id': '9'},
+        {'filename': 'Адаптация', 'entity': 'deal', 'entity_id': None, 'category_id': '19'},
+        {'filename': 'Источники_продаж', 'entity': 'item', 'entity_id': '133', 'category_id': None},
+        {'filename': 'Досье_клиента', 'entity': 'item', 'entity_id': '186', 'category_id': None},
         #{'filename': 'Проработка_по_сервисам', 'entity': 'item', 'entity_id': '150', 'category_id': None},
-        #{'filename': 'Доступы_и_файлы', 'entity': 'item', 'entity_id': '165', 'category_id': None},
+        {'filename': 'Доступы_и_файлы', 'entity': 'item', 'entity_id': '165', 'category_id': None},
         {'filename': 'Инфо', 'entity': 'item', 'entity_id': '141', 'category_id': None}
     ]
     companies = b.get_all('crm.company.list', {
