ed3716e9e67d7230750132e177e74cef56402bf9
wtfetc <ib09@list.ru>
2025-01-17 20:32:28 +0300

    Update clear_calls.py

--- clear_calls.py
+++ clear_calls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from fast_bitrix24 import Bitrix
 from authentication import authentication
+from time import sleep
 
 # Считывание файла authentication.txt
 
@@ -21,6 +22,18 @@ def main():
     if len(activities_email) != 0:
         for activity in activities_email:
             b.get_all('crm.activity.update', {'id': activity['ID'], 'fields': {'COMPLETED': 'Y'}})
+
+    activities_tasks_comments = b.get_all('crm.activity.list', {'filter': {'PROVIDER_TYPE_ID': 'TASKS_TASK_COMMENT', 'COMPLETED': 'N'}})
+    if len(activities) != 0:
+        for activity in activities:
+            if count == 100:
+                break
+            try:
+                b.get_all('crm.activity.update', {'id': activity['ID'], 'fields': {'COMPLETED': 'Y'}})
+                print(count)
+                sleep(2)
+            except:
+                sleep(2)
 
 if __name__ == '__main__':
     main()
